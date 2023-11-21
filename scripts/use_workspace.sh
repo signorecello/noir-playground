a2677648049e9b501e0d420b63d73d75837b7725
José Pedro Sousa <github@zepedro.me>
2023-11-21 21:55:56 +0000

    committing yarn

--- scripts/use_workspace.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-
-# Update the version in packages/example/package.json
-jq --arg version "$version" '.dependencies["@signorecello/noir_playground"] = "workspace:*"' ./packages/example/package.json > temp.json
-mv temp.json ./packages/example/package.json
-
-rm -rf temp.json
